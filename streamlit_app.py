633b433671f32bb6f0b71157358e918a2d9e8758
smandavasnowflake <137805712+smandavasnowflake@users.noreply.github.com>
2023-07-12 20:50:01 +0530

    Update streamlit_app.py
    adding insert table

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,5 @@ streamlit.dataframe(my_data_rows)
 #Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
